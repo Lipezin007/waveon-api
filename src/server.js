f9b81f1ebdb3d621adf49dcd5934e3b1e0278f7a
Felippe <felippeanpe@gmail.com>
2026-04-05 15:07:56 -0300

    remove .env from git

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ async function startServer() {
   try {
     await pool.query('SELECT 1');
     console.log('BANCO CONECTADO COM SUCESSO');
+    console.log('DB_USER:', process.env.DB_USER);
+    console.log('DB_PASSWORD length:', process.env.DB_PASSWORD?.length);
 
     app.listen(PORT, () => {
       console.log(`Servidor rodando na porta ${PORT}`);
