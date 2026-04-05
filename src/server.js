6b2010a4bf09b73ad575d6c3378ef3e53f367f9b
Felippe <felippeanpe@gmail.com>
2026-04-05 14:39:08 -0300

    use database url

--- src/server.js
+++ src/server.js
@@ -6,11 +6,6 @@ const PORT = process.env.PORT || 3000;
 
 async function startServer() {
   try {
-    console.log('DB_HOST:', process.env.DB_HOST);
-    console.log('DB_PORT:', process.env.DB_PORT);
-    console.log('DB_NAME:', process.env.DB_NAME);
-    console.log('DB_USER:', process.env.DB_USER);
-
     await pool.query('SELECT 1');
     console.log('BANCO CONECTADO COM SUCESSO');
 
