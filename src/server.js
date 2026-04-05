4e4b20a6dcdcf21b16fe1d4948655c04395aef7a
Felippe <felippeanpe@gmail.com>
2026-04-05 14:14:30 -0300

    test db connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
 const app = require('./app');
+const pool = require('./config/db');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+async function startServer() {
+  try {
+    await pool.query('SELECT 1');
+    console.log('🔥 BANCO CONECTADO COM SUCESSO');
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor rodando na porta ${PORT}`);
+    });
+  } catch (error) {
+    console.error('❌ ERRO AO CONECTAR NO BANCO:', error);
+  }
+}
+
+startServer();
